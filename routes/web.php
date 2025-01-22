381cc723ecd087d66aaf8185aa6dce1c672acbf2
Suryo <deaglefrenzy@gmail.com>
2025-01-22 11:03:30 +0800

    new

--- routes/web.php
+++ routes/web.php
@@ -10,34 +10,34 @@ use App\Models\Reaction;
 use Illuminate\Support\Facades\Route;
 
 //token
-Route::get('/token', function () {
-    return csrf_token();
-});
+// Route::get('/token', function () {
+//     return csrf_token();
+// });
 
 
-Route::middleware(["LoggedIn"])->group(function () {
+// Route::middleware(["LoggedIn"])->group(function () {
 
-    Route::get('/posts', [PostsController::class, 'index']);
-    Route::get('/posts/{post}', [PostsController::class, 'show']);
-    Route::post('/posts', [PostsController::class, 'store']);
-    Route::patch('/posts/{post}', [PostsController::class, 'update']);
-    Route::delete('/posts/{post}', [PostsController::class, 'destroy']);
+// Route::get('/posts', [PostsController::class, 'index']);
+//     Route::get('/posts/{post}', [PostsController::class, 'show']);
+//     Route::post('/posts', [PostsController::class, 'store']);
+//     Route::patch('/posts/{post}', [PostsController::class, 'update']);
+//     Route::delete('/posts/{post}', [PostsController::class, 'destroy']);
 
-    Route::post('/posts/{post}/tag', [PostsController::class, 'attach']);
+//     Route::post('/posts/{post}/tag', [PostsController::class, 'attach']);
 
-    Route::post('/posts/{post}/comments', [CommentController::class, 'store']);
-    Route::patch('/posts/{post}/comments/{comment}', [CommentController::class, 'update']);
-    Route::delete('/posts/{post}/comments/{comment}', [CommentController::class, 'destroy']);
+//     Route::post('/posts/{post}/comments', [CommentController::class, 'store']);
+//     Route::patch('/posts/{post}/comments/{comment}', [CommentController::class, 'update']);
+//     Route::delete('/posts/{post}/comments/{comment}', [CommentController::class, 'destroy']);
 
-    Route::post('/posts/{post}/reaction', [ReactionController::class, 'store']);
-    Route::delete('/posts/{post}/reaction/{reaction}', [ReactionController::class, 'destroy']);
+//     Route::post('/posts/{post}/reaction', [ReactionController::class, 'store']);
+//     Route::delete('/posts/{post}/reaction/{reaction}', [ReactionController::class, 'destroy']);
 
-    Route::get('/tags', [TagController::class, 'index']);
-    Route::get('/tags/{tag:name}', [TagController::class, 'show']);
-    Route::post('/tags', [TagController::class, 'store'])->middleware('Admin');
-    Route::delete('/tags', [TagController::class, 'destroy'])->middleware('Admin');
+//     Route::get('/tags', [TagController::class, 'index']);
+//     Route::get('/tags/{tag:name}', [TagController::class, 'show']);
+//     Route::post('/tags', [TagController::class, 'store'])->middleware('Admin');
+//     Route::delete('/tags', [TagController::class, 'destroy'])->middleware('Admin');
 
-    Route::post('/logout', [LoginController::class, 'destroy']);
-});
+//     Route::post('/logout', [LoginController::class, 'destroy']);
+// });
 
-Route::post('/login', [LoginController::class, 'store'])->name("login");
+// Route::post('/login', [LoginController::class, 'store'])->name("login");
